132b6a41484c07b207533e362e5fb0a7a5b2dd4f
Lebinhzzzz <lebinh14051993@gmail.com>
2025-06-13 14:03:07 +0700

    Update lo_de_chuyen_nghiep_app.py

--- lo_de_chuyen_nghiep_app.py
+++ lo_de_chuyen_nghiep_app.py
@@ -6,12 +6,10 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, db
 import streamlit.components.v1 as components
-import json
 
 # Khởi tạo Firebase từ Streamlit Secrets
 if not firebase_admin._apps:
-    firebase_json = st.secrets["FIREBASE_KEY"]
-    cred = credentials.Certificate(json.loads(firebase_json))
+    cred = credentials.Certificate(st.secrets["FIREBASE_KEY"])
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://YOUR_PROJECT_ID.firebaseio.com'
     })
